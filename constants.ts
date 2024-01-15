7cd17771de462185d1c0de682cc8c7d64a8333a9
Robspin <robin_steeman@hotmail.com>
2024-01-15 19:50:48 +0100

    env fixerino

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 import { load } from "https://deno.land/std@0.207.0/dotenv/mod.ts"
 
-const localEnv = await load()
+const localEnv = await load({ allowEmptyValues: true })
 
 export const env = {
     API_AUTHORIZATION_KEY: localEnv.API_AUTHORIZATION_KEY || Deno.env.get('API_AUTHORIZATION_KEY'),
